762ca348dc55572b4d08a3bc45a2d375b7db8617
Robin <robin@gmail.com>
2026-05-05 09:02:14 +0530

    first commit

---
+++ src/main/java/com/r2/App.java
@@ -0,0 +1,14 @@
+package com.r2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("i am Robin");
+    }
+}
