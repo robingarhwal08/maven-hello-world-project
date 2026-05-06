efca75df9865cdf15f4858448a2ea36730eb4b0e
Robin <robin@gmail.com>
2026-05-06 15:47:52 +0530

    pom set

--- src/main/java/com/r2/App.java
+++ src/main/java/com/r2/App.java
@@ -12,6 +12,8 @@ public class App
         System.out.println("i am Robin");
         System.out.println("i am using maven and github and jfrog ");
         System.out.println("relesing version 4.1");
+
         System.out.println("today i am using jenkins");
+        System.out.println("now my pom is all set ");
     }
 }
