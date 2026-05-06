aec844967cae30ad73316fcf5ad09f4dd29c8a19
Robin <robin@gmail.com>
2026-05-06 15:13:34 +0530

    jenkins me

--- src/main/java/com/r2/App.java
+++ src/main/java/com/r2/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println("i am Robin");
         System.out.println("i am using maven and github and jfrog ");
         System.out.println("relesing version 4.1");
+        System.out.println("today i am using jenkins");
     }
 }
